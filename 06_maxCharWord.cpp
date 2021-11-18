ffe1968813bc4a812e6f4c7fbecaea84f16bc597
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-18 22:07:52 +0300

    Update 06_maxCharWord.cpp

--- 06_maxCharWord.cpp
+++ 06_maxCharWord.cpp
@@ -2,8 +2,43 @@
 
 string itc_maxCharWord(string str)
 {
-	str = itc_rmFreeSpace(str);
-	if (itc_countWords(str) == 0) return "error";
+	if (str == "")
+		return "error";
+	str += " ";
+	string word = "", result = "";
+	int cnt = 0, maxCnt = 0, wordsCnt = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			if (itc_isValidWord(word))
+			{
+				wordsCnt++;
+				int signCnt = itc_sign_cnt(word);
+				if (cnt - signCnt > maxCnt)
+				{
+					maxCnt = cnt - signCnt;
+					result = itc_get_word(word);
+				}
+			}
+			cnt = 0;
+			word = "";
+		}
+		else
+		{
+			word += str[i];
+			cnt++;
+		}
+	}
+	if (wordsCnt == 0)
+		return "error";
+	return result;
+}
+
+/*string itc_maxCharWord(string str)
+{
+
+	if ((str == "") || (itc_countWords(str) == 0)) return "error";
 	long long max = 0, len = itc_len(str);
 	string temp, otv;
 	for (int i = 0; i < len; i++) {
@@ -13,4 +48,4 @@ string itc_maxCharWord(string str)
 	}
 	if (itc_len(temp) > max) otv = temp;
 	return otv;
-}
+}*/
