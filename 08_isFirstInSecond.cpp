6e666092b5c3eee295e38b51982f25460d7bf928
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-18 22:08:38 +0300

    Update 08_isFirstInSecond.cpp

--- 08_isFirstInSecond.cpp
+++ 08_isFirstInSecond.cpp
@@ -4,5 +4,5 @@ bool itc_isFirstInSecond(string s1, string s2)
 {
 	if (s1 == "") return true;
 	if (s2 == "") return false;
-	return itc_find_str(s1, s2) == -1;
+	return (itc_find_str(s1, s2) == -1);
 }
