bb705f3cb6832c9d7e6aaf58c726794aafd24ccb
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-18 22:15:33 +0300

    Update 09_Cezar.cpp

--- 09_Cezar.cpp
+++ 09_Cezar.cpp
@@ -1,6 +1,24 @@
 #include "middle_str.h"
 
-string itc_Cezar(string str, int k)
+string itc_Cezar(string str, int k) {
+	k %= 26;
+	string ans = "";
+	if (k < 0) k += 26;
+	for (int i = 0; i < itc_len(str); i++) {
+		if (str[i] >= 'a' && str[i] <= 'z') {
+			if (str[i] + k <= 'z') ans += char(int(str[i]) + k);
+			else ans += char(int(str[i]) + k - int('z') + int('a') - 1);
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z') {
+			if (str[i] + k <= 'Z') ans += char(int(str[i]) + k);
+			else ans += char(int(str[i]) + k - int('Z') + int('A') - 1);
+		}
+		else ans += str[i];
+	}
+	return ans;
+}
+
+/*string itc_Cezar(string str, int k)
 {
 	if (k == 0) return str;
 	long long len = itc_len(str);
@@ -17,4 +35,4 @@ string itc_Cezar(string str, int k)
 			else if (str[i] < 97) str[i] = 122 - (str[i] % 97); }
 	}
 	return str;
-}
+}*/
