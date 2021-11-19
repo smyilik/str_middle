d7ae008e458e61e86c0797f0ca5f2dd832df1660
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-19 17:11:16 +0300

    Update 06_maxCharWord.cpp

--- 06_maxCharWord.cpp
+++ 06_maxCharWord.cpp
@@ -1,44 +1,10 @@
 #include "middle_str.h"
 
+
 string itc_maxCharWord(string str)
 {
-	if (str == "")
-		return "error";
-	str += " ";
-	string word = "", result = "";
-	int cnt = 0, maxCnt = 0, wordsCnt = 0;
-	for (int i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ')
-		{
-			if (itc_isValidWord(word))
-			{
-				wordsCnt++;
-				int signCnt = itc_sign_cnt(word);
-				if (cnt - signCnt > maxCnt)
-				{
-					maxCnt = cnt - signCnt;
-					result = itc_get_word(word);
-				}
-			}
-			cnt = 0;
-			word = "";
-		}
-		else
-		{
-			word += str[i];
-			cnt++;
-		}
-	}
-	if (wordsCnt == 0)
-		return "error";
-	return result;
-}
 
-/*string itc_maxCharWord(string str)
-{
-
-	if ((str == "") || (itc_countWords(str) == 0)) return "error";
+	if (itc_countWords(str) <= 1) return "error";
 	long long max = 0, len = itc_len(str);
 	string temp, otv;
 	for (int i = 0; i < len; i++) {
@@ -48,4 +14,4 @@ string itc_maxCharWord(string str)
 	}
 	if (itc_len(temp) > max) otv = temp;
 	return otv;
-}*/
+}
