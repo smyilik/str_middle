f5de026ef21bdedcad12c62eb704a3257ef36299
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-19 18:54:28 +0300

    Update 09_Cezar.cpp

--- 09_Cezar.cpp
+++ 09_Cezar.cpp
@@ -1,38 +1,21 @@
 #include "middle_str.h"
 
-string itc_Cezar(string str, int k) {
-	k %= 26;
-	string ans = "";
-	if (k < 0) k += 26;
-	for (int i = 0; i < itc_len(str); i++) {
-		if (str[i] >= 'a' && str[i] <= 'z') {
-			if (str[i] + k <= 'z') ans += char(int(str[i]) + k);
-			else ans += char(int(str[i]) + k - int('z') + int('a') - 1);
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z') {
-			if (str[i] + k <= 'Z') ans += char(int(str[i]) + k);
-			else ans += char(int(str[i]) + k - int('Z') + int('A') - 1);
-		}
-		else ans += str[i];
-	}
-	return ans;
-}
-
-/*string itc_Cezar(string str, int k)
+string itc_Cezar(string str, int k)
 {
+	k %= 26;
 	if (k == 0) return str;
 	long long len = itc_len(str);
-	if ((k == 0) || (str == "")) return str;
 	for (int i = 0; i < len; i++) {
 		if ((str[i] >= 'A') && (str[i] <= 'Z')) {
-			str[i] += k;
-			if (str[i] > 90) str[i] = 65 + (str[i] % 90);
-			else if (str[i] < 65) str[i] = 90 - (str[i] % 65);
+			if ((k > 0) && (str[i] + k > 'Z')) str[i] = 'A' + k - 'Z';
+			else if ((k < 0) && (str[i] + k < 'A')) str[i] = 'Z' + str[i] - 'B' - k;
+			else str[i] += k;
 		}
 		else if ((str[i] >= 'a') && (str[i] <= 'z')) {
-			str[i] += k;
-			if (str[i] > 122) str[i] = 97 + (str[i] % 122);
-			else if (str[i] < 97) str[i] = 122 - (str[i] % 97); }
+			if ((k > 0) && (str[i] + k > 'z')) str[i] = 'a' + k - 'z';
+			else if ((k < 0) && (str[i] + k < 'a')) str[i] = 'z' + str[i] - 'b' - k;
+			else str[i] += k;
+		}
 	}
 	return str;
-}*/
+}
