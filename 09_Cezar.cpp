a931df5293cbf504dfe313adfe5f63e046de6098
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-19 20:17:50 +0300

    Update 09_Cezar.cpp

--- 09_Cezar.cpp
+++ 09_Cezar.cpp
@@ -7,12 +7,12 @@ string itc_Cezar(string str, int k)
 	long long len = itc_len(str);
 	for (int i = 0; i < len; i++) {
 		if ((str[i] >= 'A') && (str[i] <= 'Z')) {
-			if ((k > 0) && (str[i] + k > 'Z')) str[i] = 'A' + k - 'Z';
+			if ((k > 0) && (str[i] + k > 'Z')) str[i] = str[i] + 'A' + k - 'Z';
 			else if ((k < 0) && (str[i] + k < 'A')) str[i] = 'Z' + str[i] - 'B' - k;
 			else str[i] += k;
 		}
 		else if ((str[i] >= 'a') && (str[i] <= 'z')) {
-			if ((k > 0) && (str[i] + k > 'z')) str[i] = 'a' + k - 'z';
+			if ((k > 0) && (str[i] + k > 'z')) str[i] = str[i] + 'a' + k - 'z';
 			else if ((k < 0) && (str[i] + k < 'a')) str[i] = 'z' + str[i] - 'b' - k;
 			else str[i] += k;
 		}
