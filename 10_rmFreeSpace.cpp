0b7d6f7a70e9853a852b5ff2d37d8da66cc2b6ec
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-19 20:03:48 +0300

    Update 10_rmFreeSpace.cpp

--- 10_rmFreeSpace.cpp
+++ 10_rmFreeSpace.cpp
@@ -2,9 +2,12 @@
 
 string itc_rmFreeSpace(string str)
 {
-    long long len = itc_len(str);
+    long long len = itc_len(str), i;
+    if (str == "") return str;
+    else if (str[0] == ' ') i = 1;
+    else i = 0;
     string otv = "";
-    for (int i = 0; i < len; i++) {
+    for (i; i < len; i++) {
         if ((i != len - 1) && ((str[i] != ' ') || (str[i + 1] != ' '))) otv += str[i];
     }
     return otv;
