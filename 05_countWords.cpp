bfb56f577ef40e7e9c9c363e9c8d3dd53a4a0380
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-19 18:53:32 +0300

    Update 05_countWords.cpp

--- 05_countWords.cpp
+++ 05_countWords.cpp
@@ -7,7 +7,10 @@ int itc_countWords(string str)
 	else if (((str[0] >= 'A') && (str[0] <= 'Z')) || ((str[0] >= 'a') && (str[0] <= 'z'))) kol = 1;
 	else kol = 0;
 	for (int i = 1; i < len; i++) {
-		if (!(((str[i - 1] >= 'A') && (str[i - 1] <= 'Z')) || ((str[i - 1] >= 'a') && (str[i - 1] <= 'z'))) && (((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a') && (str[i] <= 'z')))) {
+		if (i + 1 != len && !(((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a') && (str[i] <= 'z'))) && (str[i] != ' ')
+			&& (((str[i - 1] >= 'A') && (str[i - 1] <= 'Z')) || ((str[i - 1] >= 'a') && (str[i - 1] <= 'z')))
+			&& (((str[i + 1] >= 'A') && (str[i + 1] <= 'Z')) || ((str[i + 1] >= 'a') && (str[i + 1] <= 'z')))) kol -= 2;
+		else if (!(((str[i - 1] >= 'A') && (str[i - 1] <= 'Z')) || ((str[i - 1] >= 'a') && (str[i - 1] <= 'z'))) && (((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a') && (str[i] <= 'z')))) {
 			kol++;
 		}
 	}
